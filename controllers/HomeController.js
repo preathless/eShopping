4a0ebdece82f7fd03083adb4c8f33de2ac27c1b1
dungnguyen <preathless@gmail.com>
2017-07-02 21:27:31 +0700

    7/2/2017

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -6,7 +6,7 @@ const getIndex = (req, res, next) => {
     .then((data) => {
       let categories = data[0];
       let users = data[1];
-      res.render('index', {title: '', categories, users});
+      res.render('frontend/index', {title: '', categories, users});
     })
     .catch((err) => {
       next(err);
